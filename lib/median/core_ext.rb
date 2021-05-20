88ff19a2c10b075e08b149ac909e24938ae50ce0
ikneg <0930019@gmail.com>
2021-05-21 05:35:56 +0900

    Made it possible to median an array even if it contains nil.

--- lib/median/core_ext.rb
+++ lib/median/core_ext.rb
@@ -1,14 +1,16 @@
 class Array
   def median
+      array = self.compact
+
       # self.compact.blank? does not detect [nil]
-      return nil if self.compact.blank? || self.map(&:class).uniq == [NilClass]
-      raise if self.map {|v| v.is_a?(Integer) or v.is_a?(Float)}.uniq != [true]
-      self_count = self.count
-      if self_count.even?
-        even_median_index = (self_count / 2)
-        return self.sort[even_median_index-1..even_median_index].sum.fdiv(2)
+      return nil if array.compact.blank? || array.map(&:class).uniq == [NilClass]
+      raise if array.map {|v| v.is_a?(Integer) or v.is_a?(Float)}.uniq != [true]
+      array_count = array.count
+      if array_count.even?
+        even_median_index = (array_count / 2)
+        return array.sort[even_median_index-1..even_median_index].sum.fdiv(2)
       else
-        return self.sort[self_count / 2]
+        return array.sort[array_count / 2]
       end
   end
 end
--- test/core_ext_test.rb
+++ test/core_ext_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class CoreExtTest < ActiveSupport::TestCase
   def test_to_median
+    assert_equal 3, [nil, 3].median
+
     assert_equal 3, [1, 3, 4, 5, 2].median
     assert_equal 76.4026, [1.11, 2.37, 150.4352, 2.32452, 2345.345, 976].median
 
